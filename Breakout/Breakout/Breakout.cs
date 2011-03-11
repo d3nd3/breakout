cd64a0ebe929d08907be19a47aeb33a6a57df62e
seangeo <seangeo@gmail.com>
2011-03-11 13:51:43 +0930

    Added the ball and some simple collision detection and response.

--- Breakout/Breakout/Breakout.cs
+++ Breakout/Breakout/Breakout.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-    /// <summary>
-    /// This is main class encapsulating the game logic
-    /// for the Breakout game.
-    /// </summary>
+/// <summary>
+/// This is main class encapsulating the game logic
+/// for the Breakout game.
+/// </summary>
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
 
@@ -23,8 +23,8 @@ namespace Breakout {
             this.spriteBatch = spriteBatch;
             this.screenWidth = spriteBatch.GraphicsDevice.Viewport.Width;
             this.screenHeight = spriteBatch.GraphicsDevice.Viewport.Height;
-            this.ball = new Ball();
             this.paddle = new Paddle(screenWidth, screenHeight);
+            this.ball = new Ball(paddle, screenWidth, screenHeight);
             this.wall = new Wall();
         }
 
@@ -48,22 +48,114 @@ namespace Breakout {
 
         internal void Update(GameTime gameTime) {
             paddle.Update(gameTime);
+            ball.Update(gameTime);
         }
     }
+
+   
 
     /// <summary
     /// Defines the ball used in a breakout game.
     /// </summary>
     class Ball {
-        public Ball() {
+        enum State {
+            Active, Dead
+        }
+
+        private State state;
+        private Paddle paddle;
+        private int screenWidth, screenHeight;
+        private Texture2D sprite;
+        
+        private Vector2 initialPosition;
+        private Vector2 position;
+
+        private Vector2 initialDirection = new Vector2(0, 1);
+        // A unit vector for the direction of the ball
+        private Vector2 direction = new Vector2(0, 1);
+
+        // in pixels per second
+        private const int initialSpeed = 150;
+        private const int speedIncrement = 75;
+        private const int maxSpeed = 1000;
+        private int speed = initialSpeed;
+
+        public Rectangle Bounds {
+            get { return new Rectangle((int)position.X, (int)position.Y, sprite.Width, sprite.Height); }
+        }
+
+        public Ball(Paddle paddle, int screenWidth, int screenHeight) {
+            this.state = State.Active;
+            this.paddle = paddle;
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+            initialPosition = new Vector2(screenWidth / 2, screenHeight / 2);
         }
 
         internal void LoadContent(ContentManager contentManager) {
-            //throw new NotImplementedException();
+            sprite = contentManager.Load<Texture2D>("ball");
+            position = initialPosition - new Vector2(sprite.Width / 2, sprite.Height / 2);
+        }
+
+        internal void Update(GameTime gameTime) {
+            if (state == State.Active) {
+                UpdatePosition(gameTime);
+            } else if (state == State.Dead && Keyboard.GetState().IsKeyDown(Keys.Space)) {
+                LaunchBall();
+            }
+        }
+
+        private void LaunchBall() {
+            state = State.Active;
+            speed = initialSpeed;
+            direction = initialDirection;
+            position = initialPosition - new Vector2(sprite.Width / 2, sprite.Height / 2);
+        }
+
+        private void UpdatePosition(GameTime gameTime) {
+            position = position + direction * (float)(speed * gameTime.ElapsedGameTime.TotalSeconds);
+
+            if (position.Y > screenHeight) {
+                state = State.Dead;
+            } else {
+                DetectCollisions();
+            }
+        }
+
+        private void DetectCollisions() {
+            if (position.Y < 0) {
+                position.Y = 0;
+                direction.Y = -direction.Y;
+            }
+
+            if (position.X < 0) {
+                position.X = 0;
+                direction.X = -direction.X;
+            } else if (position.X + sprite.Width > screenWidth) {
+                position.X = screenWidth - sprite.Height;
+                direction.X = -direction.X;
+            }
+
+            if (direction.Y > 0) {
+                if (paddle.Bounds.Intersects(this.Bounds)) {
+                    direction.Y = -direction.Y;
+                    position.Y = paddle.Bounds.Y - sprite.Height;
+
+                    // figure out the new X direction based on distance from the paddle center
+                    direction.X = (position.X - paddle.Center.X) / (paddle.Bounds.Width / 2);
+                    direction = Vector2.Normalize(direction);
+
+                    // Add a bit to the speed
+                    speed += speedIncrement;
+                    speed = Math.Min(speed, maxSpeed);
+                }
+            }
         }
 
         internal void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
-            //throw new NotImplementedException();
+            if (state == State.Active) {
+                spriteBatch.Draw(sprite, position, Color.White);
+            }
         }
     }
 
@@ -75,14 +167,14 @@ namespace Breakout {
     /// </summary>
     class Paddle {
         // The initial at which the paddle will start to move.
-        const int INITIAL_SPEED = 400;
+        const int INITIAL_SPEED = 300;
         // The factor by which the paddle's movement will
         // accelerate per second.
-        Vector2 ACCELERATION = new Vector2(1600, 0);
+        Vector2 ACCELERATION = new Vector2(2000, 0);
         const int PADDLE_HEIGHT = 24;
         // How far of the bottom of the screen paddle is drawn
         const int PADDLE_OFFSET = 10;
-        
+
         private Texture2D sprite;
         // The direction and speed at which the paddle is moving.
         private Vector2 velocity = Vector2.Zero;
@@ -108,7 +200,11 @@ namespace Breakout {
         }
 
         public Rectangle Bounds {
-            get { return new Rectangle((int) position.X, (int) position.Y, sprite.Width, PADDLE_HEIGHT); }
+            get { return new Rectangle((int)position.X, (int)position.Y, sprite.Width, PADDLE_HEIGHT); }
+        }
+
+        public Point Center {
+            get { return new Point((int)position.X + sprite.Width / 2, (int)position.Y + sprite.Height / 2); }
         }
 
         /// <summary>
@@ -117,7 +213,7 @@ namespace Breakout {
         /// <param name="spriteBatch">The sprite batch used to draw the paddle.</param>
         public Paddle(int screenWidth, int screenHeight) {
             this.screenWidth = screenWidth;
-            this.screenHeight = screenHeight; 
+            this.screenHeight = screenHeight;
             this.previousKeyboard = Keyboard.GetState();
         }
 
@@ -161,7 +257,7 @@ namespace Breakout {
 
             position = position + (velocity * time);
             position = Vector2.Clamp(position, minPosition, maxPosition);
-            
+
             previousKeyboard = keyboard;
         }
 
@@ -183,7 +279,7 @@ namespace Breakout {
         }
 
         internal void LoadContent(ContentManager contentManager) {
-           // throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
         internal void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
